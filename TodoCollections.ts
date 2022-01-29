4acf172b2cf334b7d85a60f43c8633bbea3fc407
Hyerin1208 <89626469+Hyerin1208@users.noreply.github.com>
2022-01-29 21:23:10 +0900

    Add files via upload

--- TodoCollections.ts
+++ TodoCollections.ts
@@ -3,18 +3,44 @@ import TodoItem from "./TodoItem";
 class TodoCollection {
     private nextId : number = 1;
 
-    constructor(public userName: string, public todoItems:TodoItem[] = []) {}
+    // itemMap이라는 프로퍼티에 TodoItem 저장하도록 Map 객체사용
+    private itemMap : Map<number, TodoItem>; // 제너릭 사용한 형태
+
+    constructor(public userName: string, public todoItems:TodoItem[] = []) {
+        // 생성자 내부 코드
+        this.itemMap = new Map<number, TodoItem>();
+        // id를 통해 TodoItem을 통해 꺼내는 형태
+        todoItems.forEach((item) => this.itemMap.set(item.id, item));
+
+    }
     
     getTodoById(id: number) : TodoItem | undefined {
-        return this.todoItems.find((item) => item.id === id);
+        return this.itemMap.get(id);
     }
 
     addTodo(task: string) : number {
         while(this.getTodoById(this.nextId)) {
             this.nextId++;
         }
-        this.todoItems.push(new TodoItem(this.nextId, task))
+        this.itemMap.set(this.nextId, new TodoItem(this.nextId, task))
         return this.nextId;
+    }
+
+    // includeComplete => true : 모든 할일목록 반환
+    // includeComplete => false : 완료목록은 제외한 할일 목록반환.
+    getTodoItems(includeComplete:boolean) : TodoItem[] {
+        return [...this.itemMap.values()].filter(
+            (item) => includeComplete || !item.complete
+        )
+    }
+
+    removeComplete():void {
+        this.itemMap.forEach((item) => {
+            if(item.complete){
+                // 이미 완료된 항목
+                this.itemMap.delete(item.id);
+            }
+        })
     }
 
     markComplete(id: number, complete: boolean) : void {
--- index.ts
+++ index.ts
@@ -14,4 +14,11 @@ myTodoCollection.addTodo("친구만나기")
 myTodoCollection.markComplete(3, true)
 
 console.log(`${myTodoCollection.userName}`);
-myTodoCollection.todoItems.forEach((item) => item.printDetails());
+
+myTodoCollection.removeComplete();
+
+// 위처럼 todoItem에서 꺼내지 않고 TodoCollections에서 getTodoItems에서 꺼내도록
+myTodoCollection.getTodoItems(true).forEach((item)=>item.printDetails())
+// 위 true로 준 값을 아래는 false로 나오도록
+console.log("------------------");
+myTodoCollection.getTodoItems(false).forEach((item)=>item.printDetails())
