056dd00feef992b433d30aa1b22936387a152f3d
Hyerin1208 <89626469+Hyerin1208@users.noreply.github.com>
2022-02-01 23:19:30 +0900

    Add files via upload

---
+++ src/service/TodoCollections.ts
@@ -0,0 +1,62 @@
+import { ItemCounts } from "../model/itemCounts";
+import TodoItem from "../model/TodoItem";
+
+class TodoCollection {
+    private nextId : number = 1;
+
+    // itemMap이라는 프로퍼티에 TodoItem 저장하도록 Map 객체사용
+    private itemMap : Map<number, TodoItem>; // 제너릭 사용한 형태
+
+    constructor(public userName: string, public todoItems:TodoItem[] = []) {
+        // 생성자 내부 코드
+        this.itemMap = new Map<number, TodoItem>();
+        // id를 통해 TodoItem을 통해 꺼내는 형태
+        todoItems.forEach((item) => this.itemMap.set(item.id, item));
+
+    }
+    
+    getTodoById(id: number) : TodoItem | undefined {
+        return this.itemMap.get(id);
+    }
+
+    addTodo(task: string) : number {
+        while(this.getTodoById(this.nextId)) {
+            this.nextId++;
+        }
+        this.itemMap.set(this.nextId, new TodoItem(this.nextId, task))
+        return this.nextId;
+    }
+
+    // includeComplete => true : 모든 할일목록 반환
+    // includeComplete => false : 완료목록은 제외한 할일 목록반환.
+    getTodoItems(includeComplete:boolean) : TodoItem[] {
+        return [...this.itemMap.values()].filter(
+            (item) => includeComplete || !item.complete
+        )
+    }
+
+    removeComplete():void {
+        this.itemMap.forEach((item) => {
+            if(item.complete){
+                // 이미 완료된 항목
+                this.itemMap.delete(item.id);
+            }
+        })
+    }
+
+    getItemCounts() : ItemCounts {
+        return {
+            total : this.itemMap.size,
+            incomplete : this.getTodoItems(false).length
+        }
+    }
+
+    markComplete(id: number, complete: boolean) : void {
+        const todoItem = this.getTodoById(id);
+        if(todoItem){
+            todoItem.complete = complete;
+        }
+    }
+}
+
+export default TodoCollection;
---
+++ src/view/TodoConsole.ts
@@ -0,0 +1,44 @@
+import * as inquirer from 'inquirer';
+import TodoItem from '../model/TodoItem';
+import { data }  from '../data'
+import TodoCollection from '../service/TodoCollections';
+import { Commands } from '../model/Commands';
+
+class TodoConsole {
+    private todoCollection : TodoCollection
+
+    constructor() {
+
+        const sampleTodos : TodoItem[] = data.map(
+            (item) => new TodoItem(item.id, item.task, item.complete)
+        );
+
+        this.todoCollection = new TodoCollection('My Todo List', sampleTodos)
+    }
+
+    displayTodoList():void{
+        console.log(
+            `=====${this.todoCollection.userName}=====` 
+        + `(${this.todoCollection.getItemCounts().incomplete} items todo)`
+        )
+        this.todoCollection.getTodoItems(true).forEach((item) => item.printDetails());
+    }
+
+    promptUser():void{
+        console.clear();
+        this.displayTodoList();
+
+        inquirer.prompt({
+            type: 'list',
+            name: 'command',
+            message: 'Choose option',
+            choices: Object.values(Commands),
+        }).then((answers) => {
+            if(answers['command'] !== Commands.Quit) {
+                this.promptUser();
+            }
+        })
+    }
+}
+
+export default TodoConsole;
