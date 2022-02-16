5b4021bca1b5e02b7d29d897f0668bf88cce139b
Hyerin1208 <89626469+Hyerin1208@users.noreply.github.com>
2022-02-16 20:20:30 +0900

    union

--- NamooSorter.ts
+++ NamooSorter.ts
@@ -1,20 +1,34 @@
 class NamooSorter {
-  constructor(private numbers: number[]) {}
-  sort(): number[] {
-    const { length } = this.numbers;
+  constructor(private collection: number[] | string) {}
+  sort(): number[] | string {
+    const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.numbers[j] > this.numbers[j + 1]) {
-          // swap
-          const temp = this.numbers[j];
-          this.numbers[j] = this.numbers[j + 1];
-          this.numbers[j + 1] = temp;
+        if (typeof this.collection === "string") {
+          if (
+            this.collection[j].toLowerCase() >
+            this.collection[j + 1].toLowerCase()
+          ) {
+            const characters = this.collection.split("");
+            const temp = characters[j];
+            characters[j] = characters[j + 1];
+            characters[j + 1] = temp;
+            this.collection = characters.join("");
+          }
+        }
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            // swap
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
         }
       }
     }
 
-    return this.numbers;
+    return this.collection;
   }
 }
 
--- index.ts
+++ index.ts
@@ -1,5 +1,22 @@
 import NamooSorter from "./NamooSorter";
 
-const sorter = new NamooSorter([10, -7, 22, 3, 15]);
-
+let sorter = new NamooSorter([10, -7, 22, 3, 15]);
 console.log(sorter.sort());
+
+sorter = new NamooSorter("TypeScript");
+console.log(sorter.sort());
+
+// // data라는 변수 하나 선언
+// let data : string | number [];
+
+// function isString(data: string | number[]): data is string {
+//     return( <string>data).split !== undefined;
+// }
+
+// data = 'TypeScript';
+// if(isString(data)){
+//     data.split('')
+// } else {
+//     // number[] 타입이 될것이고
+//     // 위 타입에 어떠한 작업을 진행하면 됨.
+// }
